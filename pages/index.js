7c14585f1abe008c8161eede0ab6d8799ed0352e
Gabriel <gabrielsilva291996@gmail.com>
2022-01-24 17:37:12 -0300

    Commit Inicial

---
+++ pages/index.js
@@ -0,0 +1,237 @@
+import config from '../config.json'
+import { Box, Image, Button, Text, TextField, Icon } from '@skynexui/components'
+
+const GlobalStyle = () => {
+
+    return (
+        <>
+            <style global jsx>{`
+                @import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
+            
+                *{
+                    margin: 0;
+                    padding: 0;
+                    box-sizing: border-box;
+                    list-style: none;
+                }
+
+                html, body, #__next {
+                    min-height: 100vh;
+                    display: flex;
+                    flex: 1;
+                    font-family: 'Pacifico', cursive;
+                }
+
+                #__next {
+                    flex: 1;
+                  }
+                  #__next > * {
+                    flex: 1;
+                  }
+
+            `}</style>
+        </>
+    )
+
+}
+
+const Title = ({ children, tag }) => {
+
+    console.log(children)
+
+    const Tag = tag || 'h1'
+
+    return (
+        <>
+            <style jsx>{`
+                ${Tag} {
+                    color: ${config.theme.colors.neutrals['050']};
+                    font-size: 2.188rem;
+                    font-weight: 600;
+                }
+
+                h1 {
+                    color: red;
+                    font-size: 1.5rem;
+                    font-weight: 600;
+                }
+            `}
+            </style>
+
+            <Tag>{children}</Tag>
+
+        </>
+    )
+}
+
+
+const HomePage = () => {
+
+    const username = 'SilvasGabriel'
+
+    return (
+        <>
+            <GlobalStyle />
+            <Box
+                styleSheet={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    backgroundColor: config.theme.colors.neutrals['000'],
+                    backgroundImage: 'url(https://images.unsplash.com/photo-1597318181409-cf64d0b5d8a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80)',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'cover',
+                    backgroundBlendMode: 'multiply',
+                }}
+            >
+                <Box
+                    styleSheet={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'space-between',
+                        flexDirection: {
+                            xs: 'column',
+                            sm: 'row',
+                        },
+                        width: '100%',
+                        maxWidth: '43.75rem',
+                        borderRadius: '0.3125rem',
+                        padding: '2rem',
+                        margin: '1rem',
+                        boxShadow: ' 0 0.125rem 0.625rem 0 rgb( 0 0 0 / 20%)',
+                        backgroundColor: config.theme.colors.neutrals[700],
+                    }}
+                >
+                    {/** Formulário */}
+                    <Box as="form"
+                        styleSheet={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            width: {
+                                xs: '100%',
+                                sm: '50%',
+                            },
+                            textAlign: 'center',
+                            marginBottom: '2rem',
+                        }}
+                    >
+
+                        <Title tag="h2">
+                            <Icon
+                                label="Icon Component"
+                                name="FaMugHot"
+                                size="4ch"
+                            />  
+                        </Title>    
+
+                        <Text
+                            vatiant="body3"
+                            styleSheet={{
+                                fontFamily: '',
+                                fontSize: '1.5rem',
+                                marginTop: '2rem',
+                                marginBottom: '2rem',
+                                color: config.theme.colors.neutrals['000'],
+                            }}>
+
+                            {config.name}
+
+                        </Text>
+
+                        <TextField
+                            fullWidth
+                            textFieldColors={{
+                                neutral: {
+                                    textColor: config.theme.colors.neutrals[900],
+                                    mainColor: config.theme.colors.neutrals[800],
+                                    mainColorHighlight: config.theme.colors.primary[800],
+                                    backgroundColor: config.theme.colors.neutrals['050'],
+                                }
+                            }}
+                            styleSheet={{
+                                fontFamily: 'Open Sans, sans-serif',
+                            }}
+                        />
+
+                        <Button
+                            type='submit'
+                            label='Entrar'
+                            fullWidth
+                            buttonColors={{
+                                contrastColor: config.theme.colors.neutrals['000'],
+                                mainColor: config.theme.colors.primary[800],
+                                mainColorLight: config.theme.colors.primary[700],
+                                mainColorStrong: config.theme.colors.primary[900],
+                            }}
+
+                        />
+
+                    </Box>
+                    {/** Formulário */}
+
+                    {/*Área da Foto*/}
+
+                    <Box 
+                        styleSheet={{
+                            display:'flex',
+                            flexDirection:'column',
+                            alignItems:'center',
+                            maxWidth:'12.5rem',
+                            padding:'1rem',
+                            backgroundColor: config.theme.colors.neutrals[700],
+                            border: '0.063rem solid',
+                            borderColor: config.theme.colors.primary['050'],
+                            borderRadius: '0.625rem',
+                            flex: 1,
+                            minHeigth: '15rem',
+                        }}
+                    >
+                    
+                        <Image
+                            styleSheet={{
+                                borderRadius: '50%',
+                                marginBottom: '1rem',
+                            }}
+                            src={`https://github.com/${username}.png`}
+                        />
+
+                        <Text
+                            variant="body4"
+                            styleSheet={{
+                                color: config.theme.colors.neutrals['100'],
+                                backgroundColor: config.theme.colors.neutrals[700],
+                                padding: '0.1875rem 0.625rem',
+                                borderRadius: '62.5rem',
+                                fontSize: '1.4rem',
+                                fontFamily:'',
+                            }}
+                        >
+                            {username}
+                        </Text>
+
+                    </Box>
+
+                </Box>
+
+            </Box>
+        </>
+    )
+}
+
+export default HomePage
+
+/*
+const HomePage = () => {
+    return (
+        <div>
+            <GlobalStyle />
+            <Title tag="h2">Bem vindos!</Title>
+            <h2>Chá_T - Conversas sobre chás</h2>
+        </div>
+    );
+};
+
+export default HomePage;
+*/
